4da72e771fe522f396e5c67fa801319e80efb862
naoto0822 <n.h.in.m.h@gmail.com>
2017-12-27 02:42:40 +0900

    tmp commit

--- examples/main.go
+++ examples/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"github.com/naoto0822/ltsv-go/ltsv"
 )
 
+type Repository struct {
+	ID          int                    `ltsv:"id"`
+	Name        string                 `ltsv:"name"`
+	Description string                 `ltsv:"description"`
+	URL         string                 `ltsv:"url"`
+	Private     bool                   `ltsv:"private"`
+	Topics      []string               `ltsv:"topics"`
+	Owner       map[string]interface{} `ltsv:"owner"`
+}
+
 func main() {
-	fmt.Println("ltsv-go!")
+	repo := Repository{
+		ID:          123,
+		Name:        "ltsv-go",
+		Description: "ltsv Marshal and Unmarshal",
+		URL:         "http://google.com",
+	}
+
+	ltsv.Marshal(repo)
 }
--- ltsv/ltsv.go
+++ ltsv/ltsv.go
@@ -2,12 +2,27 @@ package ltsv
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func Marshal(v interface{}) (string, error) {
-	// TODO:
+	rv := reflect.ValueOf(v)
+	rt := rv.Type()
+
+	//var pairs []string
+	for i := 0; i < rt.NumField(); i++ {
+		key := rt.Field(i).Tag.Get("ltsv")
+		value := rv.Field(i).Interface()
+
+		fmt.Println(key)
+		fmt.Println(value)
+		fmt.Println("---")
+	}
+
+	return "", nil
 }
 
 func Unmarshal(log string, v interface{}) error {
 	// TODO:
+	return nil
 }
